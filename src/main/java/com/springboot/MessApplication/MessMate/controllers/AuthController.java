98d56a2df2d4f860d36d53b466a7faac9aaf42fb
Karan Chhabda <karanchhabdakc29@gmail.com>
2025-06-22 18:16:46 +0530

    some changes

--- src/main/java/com/springboot/MessApplication/MessMate/controllers/AuthController.java
+++ src/main/java/com/springboot/MessApplication/MessMate/controllers/AuthController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-//    @PostMapping("/signup")
-//    public ResponseEntity<UserDto> signup() {}
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> signup() {
+        return ResponseEntity.ok(new UserDto());
+    }
 
 }
